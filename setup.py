e751fb6b9ddc6c1e7292195801ba0274ee3ce7a8
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2016-10-16 16:28:54 +0200

    Requirements & setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='recruitment-challenge',
+    version='',
+    packages=['src', 'src.tools'],
+    url='',
+    license='',
+    author='gszpak',
+    author_email='',
+    description=''
+)
